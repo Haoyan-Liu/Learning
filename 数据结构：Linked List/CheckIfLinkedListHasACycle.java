4b3535dbf834f1670846b74c8bdd3829e474aeb4
Haoyan-Liu <51977598+Haoyan-Liu@users.noreply.github.com>
2025-03-09 22:01:35 -0400

    Create CheckIfLinkedListHasACycle.java

---
+++ 数据结构：Linked List/CheckIfLinkedListHasACycle.java
@@ -0,0 +1,27 @@
+/**
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+public class Solution {
+  public boolean hasCycle(ListNode head) {
+    // write your solution here
+    ListNode dummy = new ListNode(-1);
+    dummy.next = head;
+    ListNode slow = dummy;
+    ListNode fast = dummy;
+    while (fast.next != null && fast.next.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (slow == fast) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
