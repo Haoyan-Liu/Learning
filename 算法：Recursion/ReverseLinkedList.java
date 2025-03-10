b3b30e6843b5963625b436673c18008a0e3b190c
Haoyan-Liu <51977598+Haoyan-Liu@users.noreply.github.com>
2025-03-09 22:24:44 -0400

    Create ReverseLinkedList.java

---
+++ 算法：Recursion/ReverseLinkedList.java
@@ -0,0 +1,12 @@
+public class Solution {
+  public ListNode reverse(ListNode head) {
+    // Write your solution here
+    if (head == null || head.next == null) {
+      return head;
+    }
+    ListNode newHead = reverse(head.next);
+    head.next.next = head;
+    head.next = null;
+    return newHead;
+  }
+}
