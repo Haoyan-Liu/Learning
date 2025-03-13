ee2f9a552b92cdb0ab4a159c615ef640eb85c7a4
Haoyan-Liu <51977598+Haoyan-Liu@users.noreply.github.com>
2025-03-12 22:41:21 -0400

    Update ReverseString.java

--- 数据结构：String/ReverseString.java
+++ 数据结构：String/ReverseString.java
@@ -1,1 +1,16 @@
-public
+public class Solution {
+  public String reverse(String input) {
+    // Write your solution here
+    char[] array = input.toCharArray();
+    int i = 0;
+    int j = array.length - 1;
+    while(i < j) {
+      char tmp = array[i];
+      array[i] = array[j];
+      array[j] = tmp;
+      i++;
+      j--;
+    }
+    return new String(array);
+  }
+}
