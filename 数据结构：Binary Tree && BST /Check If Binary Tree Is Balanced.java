957d1b5aa0c8f33da2c6d60faa925df1ad419203
Haoyan-Liu <51977598+Haoyan-Liu@users.noreply.github.com>
2025-03-16 22:45:14 -0400

    Update Check If Binary Tree Is Balanced.java

--- 数据结构：Binary Tree && BST /Check If Binary Tree Is Balanced.java
+++ 数据结构：Binary Tree && BST /Check If Binary Tree Is Balanced.java
@@ -1,3 +1,10 @@
+// base
+// balanced
+// TC：每一层O(n)一共log(n)层
+// SC：log(n)
+// not balanced（糖葫芦）
+// TC：O(n)
+// SC：O(n)
 public class Solution {
   public boolean isBalanced(TreeNode root) {
     // Write your solution here
@@ -17,5 +24,32 @@ public class Solution {
       return 0;
     }
     return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+  }
+}
+
+// Advanced
+// TC：O(n)
+// SC：O(h)
+public class Solution {
+  public boolean isBalanced(TreeNode root) {
+    return checkHeight(root) != -1;
+  }
+
+  private int checkHeight(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    int leftHeight = checkHeight(root.left);
+    if (leftHeight == -1) {
+      return -1;
+    } 
+    int rightHeight = checkHeight(root.right);
+    if (rightHeight == -1) {
+      return -1;
+    }
+    if (Math.abs(leftHeight - rightHeight) > 1) {
+      return -1;
+    }
+    return Math.max(leftHeight, rightHeight) + 1;
   }
 }
