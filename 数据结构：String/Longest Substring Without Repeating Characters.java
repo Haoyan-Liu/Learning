41ae2c97ff40c7a1f760170bb7fe884f67d8f25b
Haoyan-Liu <51977598+Haoyan-Liu@users.noreply.github.com>
2025-04-02 20:57:29 -0400

    Update Longest Substring Without Repeating Characters.java

--- 数据结构：String/Longest Substring Without Repeating Characters.java
+++ 数据结构：String/Longest Substring Without Repeating Characters.java
@@ -1,1 +1,28 @@
-public
+public class Solution {
+  public int longest(String input) {
+    // Write your solution here
+    int maxLen = 0;
+    // int curLen = 0;
+    int slow = 0;
+    int fast = 0;
+    Set<Character> exist = new HashSet<>();
+    while (fast < input.length()) {
+      if (exist.contains(input.charAt(fast))) {
+        while(input.charAt(slow) != input.charAt(fast)) {
+          exist.remove(input.charAt(slow));
+          slow++;
+          // curLen--;
+        }
+        slow++;
+        fast++;
+        maxLen = Math.max(maxLen, fast - slow);
+      } else {
+        exist.add(input.charAt(fast));
+        // curLen++;
+        fast++;
+        maxLen = Math.max(maxLen, fast - slow);
+      }
+    }
+    return maxLen;
+  }
+}
